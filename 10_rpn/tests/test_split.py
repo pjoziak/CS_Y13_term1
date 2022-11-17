42352937135aa62eb7e31820d1cb998de7624407
Paweł Józiak <pjoziak@gmail.com>
2022-11-17 12:57:28 +0100

    ddt instead of lengthy human loop

--- 10_rpn/tests/test_split.py
+++ 10_rpn/tests/test_split.py
@@ -1,28 +1,19 @@
 import unittest
+from ddt import ddt, data, unpack
 
 from rpn.core.utils import split_infix
 
-
+@ddt
 class SplitInfixTestCase(unittest.TestCase):
-    infix_0 = "1 * 2 - 3"
-    splitted_0 = ["1", "*", "2", "-", "3"]
-
-    infix_1 = "(7 + 10 / 5) * (6 + 2)"
-    splitted_1 = ["(", "7", "+", "10", "/", "5", ")", "*", "(", "6", "+", "2", ")"]
-
-    infix_2 = "6 * (3 - (3 - 2 / 1)) + 1"
-    splitted_2 = ["6", "*", "(", "3", "-", "(", "3", "-", "2", "/", "1", ")", ")", "+", "1"]
-
-    infix_3 = "2^3 - 6 / 2"
-    splitted_3 = ["2", "^", "3", "-", "6", "/", "2"]
-
-    def test_split_0(self):
-        splitted = split_infix(self.infix_0)
-        self.assertEqual(splitted, self.splitted_0)
-
-    def test_split_1(self):
-        splitted = split_infix(self.infix_1)
-        self.assertEqual(splitted, self.splitted_1)
+    @data(
+        ["1 * 2 - 3", ["1", "*", "2", "-", "3"]],
+        ["(7 + 10 / 5) * (6 + 2)", ["(", "7", "+", "10", "/", "5", ")", "*", "(", "6", "+", "2", ")"]],
+        ["6 * (3 - (3 - 2 / 1)) + 1", ["6", "*", "(", "3", "-", "(", "3", "-", "2", "/", "1", ")", ")", "+", "1"]],
+        ["2^3 - 6 / 2", ["2", "^", "3", "-", "6", "/", "2"]]
+    )
+    @unpack
+    def test_split(self, infix, splitted):
+        self.assertEqual(split_infix(infix), splitted)
 
 
 if __name__ == '__main__':
